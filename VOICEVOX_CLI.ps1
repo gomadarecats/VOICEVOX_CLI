83a6f54de6b7c89ef1ef45edb3eb28609fcec190
Egoma <gomadarecats@users.noreply.github.com>
2024-01-10 00:30:38 +0900

    Update VOICEVOX_CLI.ps1

--- VOICEVOX_CLI.ps1
+++ VOICEVOX_CLI.ps1
@@ -1,10 +1,15 @@
 Param(
   [parameter(mandatory=$true)][string]$text,
   [int32]$speaker = 1,
-  [string]$vvoxhost = "localhost",
+  [string]$vvoxhost = "127.0.0.1",
   [string]$outpath = [System.IO.Path]::GetTempPath()+"vvoxout_"+(Get-Date -Format "yyyymmdd_HHmmss")+".wav",
   [switch]$save
 )
+$testcon = Test-NetConnection $vvoxhost -Port 50021 -InformationLevel Quiet 3> OUT-Null
+if ($testcon -eq $false) {
+  echo "Can not connect to VOICEVOX host."
+  exit 1
+}
 
 $aquri = "http://$vvoxhost"+":50021/audio_query?text=$text&speaker=$speaker"
 $syuri = "http://$vvoxhost"+":50021/synthesis?speaker=$speaker"
